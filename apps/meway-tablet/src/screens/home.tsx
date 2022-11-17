c8fdae0925e37ab7c9351179098831b66636166c
chandan tiwari <developerchandan1@gmail.com>
2022-11-18 00:45:13 +0530

    fix: fix issues

--- apps/meway-tablet/src/screens/home.tsx
+++ apps/meway-tablet/src/screens/home.tsx
@@ -9,62 +9,8 @@ import {
 } from "react-native";
 
 import { FlashList } from "@shopify/flash-list";
-import type { inferProcedureOutput } from "@trpc/server";
-import type { AppRouter } from "@meway/api";
-
-import { trpc } from "../utils/trpc";
-
-const PostCard: React.FC<{
-  post: inferProcedureOutput<AppRouter["post"]["all"]>[number];
-}> = ({ post }) => {
-  return (
-    <View className="p-4 border-2 border-gray-500 rounded-lg">
-      <Text className="text-xl font-semibold text-gray-800">{post.title}</Text>
-      <Text className="text-gray-600">{post.content}</Text>
-    </View>
-  );
-};
-
-const CreatePost: React.FC = () => {
-  const utils = trpc.useContext();
-  const { mutate } = trpc.post.create.useMutation({
-    async onSuccess() {
-      await utils.post.all.invalidate();
-    },
-  });
-
-  const [title, onChangeTitle] = React.useState("");
-  const [content, onChangeContent] = React.useState("");
-
-  return (
-    <View className="p-4 border-t-2 border-gray-500 flex flex-col">
-      <TextInput
-        className="border-2 border-gray-500 rounded p-2 mb-2"
-        onChangeText={onChangeTitle}
-        placeholder="Title"
-      />
-      <TextInput
-        className="border-2 border-gray-500 rounded p-2 mb-2"
-        onChangeText={onChangeContent}
-        placeholder="Content"
-      />
-      <TouchableOpacity
-        className="bg-indigo-500 rounded p-2"
-        onPress={() => {
-          mutate({
-            title,
-            content,
-          });
-        }}
-      >
-        <Text className="text-white font-semibold">Publish post</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
 
 export const HomeScreen = () => {
-  const postQuery = trpc.post.all.useQuery();
   const [showPost, setShowPost] = React.useState<string | null>(null);
 
   return (
@@ -84,20 +30,7 @@ export const HomeScreen = () => {
             <Text className="italic font-semibold">Press on a post</Text>
           )}
         </View>
-
-        <FlashList
-          data={postQuery.data}
-          estimatedItemSize={20}
-          ItemSeparatorComponent={() => <View className="h-2" />}
-          renderItem={(p: { item: { id: any } }) => (
-            <TouchableOpacity onPress={() => setShowPost(p.item.id)}>
-              <PostCard post={p.item} />
-            </TouchableOpacity>
-          )}
-        />
-
-        <CreatePost />
       </View>
     </SafeAreaView>
-  );
-};
+
+)};
