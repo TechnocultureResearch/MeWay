b153e6e36a236796fbd39e3fc731329af1d94295
chandan tiwari <developerchandan1@gmail.com>
2022-11-25 19:50:37 +0530

    fix:index.ts entry point

--- apps/meway-tablet/index.ts
+++ apps/meway-tablet/index.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 
 import { registerRootComponent } from "expo";
-import {App} from "./src/_app"; /* CHANGE THE PATH BASED ON WHERE YOURS IS LOCATED */
+import { App } from "./src/_app"; /* CHANGE THE PATH BASED ON WHERE YOURS IS LOCATED */
 registerRootComponent(App);
