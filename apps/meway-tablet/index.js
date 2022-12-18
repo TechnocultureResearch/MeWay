a61b0af4eb334af436bcdb94bd5726e243b8bfa6
chandan tiwari <developerchandan1@gmail.com>
2022-12-19 02:42:03 +0530

    feat:add sentry to the project

--- apps/meway-tablet/index.js
+++ apps/meway-tablet/index.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-undef */
-
+import * as Sentry from 'sentry-expo';
+Sentry.init({
+  dsn: 'https://321935807a544823a2ce6ecfcdac2f67@o1430003.ingest.sentry.io/4504351639470080',
+  enableInExpoDevelopment: true,
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
 import { registerRootComponent } from "expo";
 import { App } from "./src/_app"; /* CHANGE THE PATH BASED ON WHERE YOURS IS LOCATED */
-registerRootComponent(App);
+registerRootComponent(Sentry.Native.wrap(App));
--- apps/meway-tablet/src/_app.tsx
+++ apps/meway-tablet/src/_app.tsx
@@ -5,12 +5,14 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { HomeScreen } from "./screens/home";
 import { FaceCount } from "./screens/FaceCount";
 const Stack = createStackNavigator();
-
+import * as Sentry from "sentry-expo";
 export const App = () => {
+  Sentry.Native.nativeCrash();
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="home" detachInactiveScreens>
         <Stack.Screen name="Home" component={HomeScreen} />
+        
         <Stack.Screen name="FaceCount" component={FaceCount} />
       </Stack.Navigator>
     </NavigationContainer>
