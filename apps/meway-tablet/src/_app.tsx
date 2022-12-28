5ffe101aa3b60a57d3d6deab6bf5df563bd867b8
Naman Rajput <namanrajput801@gmail.com>
2022-12-28 19:44:42 +0530

    chore: changes in App file

--- apps/meway-tablet/src/_app.tsx
+++ apps/meway-tablet/src/_app.tsx
@@ -1,27 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { HomeScreen } from "./screens/home";
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  withTiming,
-} from "react-native-reanimated";
 export const App = () => {
-  const progress = useSharedValue(0);
-
-  const reAnimatedStyle = useAnimatedStyle(() => {
-    return {
-      opacity: progress.value,
-    };
-  }, []);
-  useEffect(() => {
-    progress.value = withTiming(1, { duration: 2000 });
-  });
   return (
     <SafeAreaView>
-      <Animated.View className={`bg-black`} style={[{}, reAnimatedStyle]}>
+      <View className={`bg-black`}>
         <HomeScreen />
-      </Animated.View>
+      </View>
     </SafeAreaView>
   );
 };
